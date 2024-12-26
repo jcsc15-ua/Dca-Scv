2c4102fcbb6285cad0fc1408c4892e6d107823ea
Javier Clement Serna <jcsc15@alu.ua.es>
2024-12-26 12:36:15 +0100

    Otro cambio que no corrige el error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py (introducir fallo)
 def suma(a, b):
-    return a - b  # ERROR: Lógica incorrecta
+    return a * b  # ERROR: Lógica incorrecta
 
 if __name__ == "__main__":
     print(suma(2, 3))  # Esto imprimirá -1, no 5
