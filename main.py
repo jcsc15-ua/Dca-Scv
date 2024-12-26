8ff41f787dccab64c216798df07c302135d36aee
Javier Clement Serna <jcsc15@alu.ua.es>
2024-12-26 12:35:35 +0100

    Modificar suma() con error intencionado

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+# main.py (introducir fallo)
 def suma(a, b):
-    return a + b
+    return a - b  # ERROR: Lógica incorrecta
 
 if __name__ == "__main__":
-    print(suma(2, 3))  # Esto debería imprimir 5
+    print(suma(2, 3))  # Esto imprimirá -1, no 5
